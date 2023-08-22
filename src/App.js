1f58885f4eaf857b1744f1ff14c2fa9ca03bc95a
ovovvvvv <ovovvvvv@naver.com>
2023-08-22 17:05:13 +0900

    &&로 조건부 연산. false값을 null로 반환

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 function App() {
   const name = "리액투";
-  return (
-    <>
-      {name === "리액트" ? <h1>리액트입니다</h1> : <h1>리액트가 아닙니다.</h1>}
-    </>
-  );
+  return <>{name === "리액트" && <h1>리액트입니다</h1>}</>;
 }
 
 export default App;
