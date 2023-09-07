ec5c86ac969a6d8a3535859a26f0d58985efaef6
ovovvvvv <ovovvvvv@naver.com>
2023-09-07 13:28:16 +0900

    Property Initializer Syntax를 사용한 메서드 작성

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,6 +1,23 @@
 import { Component } from "react";
 
 class EventPractice extends Component {
+  state = {
+    message: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  };
+
   render() {
     return (
       <div>
@@ -9,10 +26,10 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
-          onChange={(e) => {
-            console.log(e.target.value);
-          }}
+          value={this.state.message}
+          onChange={this.handleChange}
         />
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
