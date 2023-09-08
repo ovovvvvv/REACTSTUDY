a5196a17f58933aaf211a43013579c937b3bc66e
ovovvvvv <ovovvvvv@naver.com>
2023-09-08 15:44:45 +0900

    input에 ref달기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+import { Component } from "react";
 
-const App = () => {
-  return <EventPractice />;
-};
-
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
 export default App;
