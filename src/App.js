9fc55b7b767e51e567c21adab7aaa55a5ba42d9d
ovovvvvv <ovovvvvv@naver.com>
2023-09-08 16:45:36 +0900

    데이터 배열을 컴포넌트 배열로 변경 map 사용! (key, index)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import IterationSample from "./IterationSample";
 import ScrollBox from "./ScrollBox";
 import { Component } from "react";
 
@@ -5,10 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
+        <IterationSample />
       </div>
     );
   }
