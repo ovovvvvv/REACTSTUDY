79d50ee6680ce76da71b80102e728b63ca1064d6
ovovvvvv <ovovvvvv@naver.com>
2023-09-12 16:00:52 +0900

    라이프사이클 메서드 사용하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import IterationSample from "./IterationSample";
-import ScrollBox from "./ScrollBox";
+import LifeCycleSample from "./LifeCycleSample";
+
 import { Component } from "react";
 
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
-        <IterationSample />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
       </div>
     );
   }
 }
+
 export default App;
